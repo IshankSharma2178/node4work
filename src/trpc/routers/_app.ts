f70568e10888e6f1da9db85c52d88a766d748d40
IshankSharma2178 <ishank.sharma.9891@gmail.com>
2025-11-16 19:45:25 +0530

    06 : background jobs

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,12 +1,25 @@
-import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
+import { inngest } from "@/inngest/client";
+import { createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
 
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    return prisma.user.findMany({
-      where: {
-        id: ctx.auth.user.id,
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    const workflow = await prisma.workflow.create({
+      data: {
+        name: `Workflow ${new Date().toISOString()}`,
       },
     });
+
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "Ishank@example.com",
+      },
+    });
+
+    return workflow;
   }),
 });
