b35a532316a143c1a385e04d65d982fb0fd96df7
IshankSharma2178 <ishank.sharma.9891@gmail.com>
2026-04-16 23:39:31 +0530

    inngest fixes

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -4,5 +4,7 @@ import { realtimeMiddleware } from "@inngest/realtime/middleware";
 // Create a client to send and receive events
 export const inngest = new Inngest({
   id: "node4work",
+  eventKey: process.env.INNGEST_EVENT_KEY,
+  signingKey: process.env.INNGEST_SIGNING_KEY,
   middleware: [realtimeMiddleware()],
 });
